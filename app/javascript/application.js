4155a23ac856112177530e51c76038b8f70abc36
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-06 13:57:55 +0330

    Add root view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function App() {
+  return (<h1>Hello</h1>)
+}
+
+ReactDom.render(
+  <App/>,
+  document.getElementById('root'),
+);

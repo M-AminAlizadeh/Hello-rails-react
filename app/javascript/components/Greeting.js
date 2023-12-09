499fe623d40119a521891dfbf8763af4df549c18
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-09 10:06:44 +0330

    Edit greeting component to fetch data from api

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { setGreeting } from '../greetingSlice';
 
-const Greeting = () => <p>inside greeting component</p>;
+const Greeting = ({ greeting, setGreeting }) => {
+  useEffect(() => {
+    fetch('http://[::1]:3000/api/random_greeting')
+      .then((response) => response.json())
+      .then((data) => setGreeting(data.greeting))
+      .catch((error) => console.error('Error fetching greeting:', error));
+  }, [setGreeting]);
 
-export default Greeting;
+  return (
+    <div>
+      <h1>Welcome to this simple app!</h1>
+      <h1>{greeting}</h1>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  greeting: state.greeting,
+});
+
+export default connect(mapStateToProps, { setGreeting })(Greeting);
